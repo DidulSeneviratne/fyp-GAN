4b72eb37f7d35a36f339048a1440c888991ee3dd
Didul Seneviratne <dulsaradidul@gmail.com>
2025-04-01 23:42:09 +0530

    ci/cd change2

--- backend/test_app.py
+++ backend/test_app.py
@@ -8,5 +8,17 @@ def test_root():
     assert response.status_code == 404  # Since there's no root endpoint
 
 def test_generate_ui():
-    response = client.post("/api/generate-ui", data={})
+    with open("../dataset1/1.jpg", "rb") as image_file:
+        response = client.post("/api/generate-ui", files={
+            "sketch": ("sketch.jpg", image_file, "image/jpg"),
+        }, data={
+            "region": "South Asia",
+            "age": "Teen",
+            "device": "Mobile",
+            "product": "Travel",
+            "useCustomColor": "false",
+            "colors1": "[]",
+            "orientation": "Portrait"
+        })
+
     assert response.status_code in [200, 400, 404]  # Adjust based on real API behavior
